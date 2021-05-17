370b75c07d357d4af29a266fa660e95559975664
JarradPrice <JarradPrice@users.noreply.github.com>
2021-05-17 23:24:40 +1000

    added simple auth to post function

--- api/messages-post/index.js
+++ api/messages-post/index.js
@@ -1,16 +1,24 @@
 const data = require('../shared/message-data');
 
-module.exports = async function (context, req) {
-  const message = {
-    id: undefined,
-    answer: req.body.answer,
-    time_taken: req.body.time_taken,
-  };
+let check = "1894577348";
 
-  try {
-    const newMessage = data.addMessage(message);
-    context.res.status(201).json(newMessage);
-  } catch (error) {
-    context.res.status(500).send(error);
+module.exports = async function (context, req) {
+  // simple auth check
+  let key = req.body.key;
+  if (key != check) {
+    context.res.status(401).send();
+  } else {
+    const message = {
+      id: undefined,
+      answer: req.body.answer,
+      time_taken: req.body.time_taken,
+    };
+  
+    try {
+      const newMessage = data.addMessage(message);
+      context.res.status(201).json(newMessage);
+    } catch (error) {
+      context.res.status(500).send(error);
+    }
   }
 };
